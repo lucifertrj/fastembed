9dda00134b85e4c7f4fcf430ea8e2eac53487b72
lucifertrj <jaintarun.abd17@gmail.com>
2026-02-05 22:46:59 +0530

    replace the hf model name

--- fastembed/sparse/bge_m3.py
+++ fastembed/sparse/bge_m3.py
@@ -20,7 +20,7 @@ supported_bge_m3_models: list[SparseModelDescription] = [
         ),
         license="mit",
         size_in_GB=2.27,
-        sources=ModelSource(hf="aapot/bge-m3-onnx"),
+        sources=ModelSource(hf="BAAI/bge-m3"),
         model_file="model.onnx",
         additional_files=["onnx/model.onnx_data", "onnx/sentencepiece.bpe.model"],
     ),
